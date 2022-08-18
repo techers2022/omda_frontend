8ff88739787b574ac275d84fba3a6b242c0cd88a
TECHers DPIT2022 <109172614+techers2022@users.noreply.github.com>
2022-08-18 17:45:17 +0300

    Created the folder structure and the main pages of the app (#2)
    * added test login page
    
    * added request to local api when login button is pressed
    
    * created all main pages
    
    Co-authored-by: George Catalin Balota <george.balota@fortech.ro>

---
+++ lib/src/features/get-work/widgets/detailed-view-get-work-page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DetailedViewGetWorkPage extends StatelessWidget {
+  const DetailedViewGetWorkPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Detailed View Work Page")),
+    );
+  }
+}
---
+++ lib/test-list.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:omda_frontend/api.service.dart';
+import 'package:omda_frontend/user.model.dart';
+
+class TestList extends StatefulWidget {
+  const TestList({super.key});
+
+  @override
+  _TestListState createState() => _TestListState();
+}
+
+class _TestListState extends State<TestList> {
+  late List<User>? _userModel = [];
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  void _getData() async {
+    _userModel = (await ApiService().getUsers())!;
+    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('REST API Example'),
+      ),
+      body: _userModel == null || _userModel!.isEmpty
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _userModel!.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(_userModel![index].id.toString()),
+                          Text(_userModel![index].username),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(_userModel![index].email),
+                          Text(_userModel![index].website),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
